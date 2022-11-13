fc2bca987c9ed1ae9c294955c740917ccd209c00
huang0429 <iswork.huang@gmail.com>
2022-11-13 10:30:56 +0800

    新增#1 簡單的table view

---
+++ #1-SimpleTable/SimpleTable/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  SimpleTable
+//
+//  Created by 黃筱珮 on 2022/11/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+    var images = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "datacell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+//        cell.imageView?.image = UIImage(named: "images")//單張照片
+        cell.imageView?.image = UIImage(named: images[indexPath.row])//多張照片
+        return cell
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    //隱藏狀態列
+    override var prefersStatusBarHidden: Bool{
+        return true
+    }
+}
+
